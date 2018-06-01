b305725475035d17dd7677e133e0ca938ba9560a
Niveditha Venugopal <nivedit@pdx.edu>
2018-06-01 10:25:04 -0700

    Plot svm code check in

--- test.py
+++ test.py
@@ -1,8 +1,73 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.decomposition import PCA
 
-train_data = np.load('data/train_encoded_array.npy')
-test_data = np.loadtxt('data/test_target_array')
-train_target = np.load('data/train_target_array.npy')
-# print(np.shape(train_data))
-print(np.shape(train_data))
-print(np.shape(train_target))
+
+
+if __name__=="__main__":
+    X=np.load('data/train_encoded_array.npy')
+    Y = np.load('data/train_target_array.npy')
+    Y = Y.astype('int')
+    Y = Y.flatten()
+    pca = PCA(n_components=2).fit(X)
+    X = pca.transform(X)
+    # t = np.array([0,1,0,0,0,0,1,1,1,1])
+    # we create an instance of SVM and fit out data. We do not scale our
+    # data since we want to plot the support vectors
+    C = 5.0  # SVM regularization parameter
+    #
+    clf = (svm.SVC(kernel='linear',C=C))
+    #
+    clf.fit(X, Y)
+
+    fignum = 1
+
+    # get the separating hyperplane
+    w = clf.coef_[0]
+    a = -w[0] / w[1]
+    xx = np.linspace(-5, 5)
+    yy = a * xx - (clf.intercept_[0]) / w[1]
+
+    # plot the parallels to the separating hyperplane that pass through the
+    # support vectors (margin away from hyperplane in direction
+    # perpendicular to hyperplane). This is sqrt(1+a^2) away vertically in
+    # 2-d.
+    margin = 1 / np.sqrt(np.sum(clf.coef_ ** 2))
+    yy_down = yy - np.sqrt(1 + a ** 2) * margin
+    yy_up = yy + np.sqrt(1 + a ** 2) * margin
+
+    # plot the line, the points, and the nearest vectors to the plane
+    plt.figure(fignum, figsize=(4, 3))
+    plt.clf()
+    plt.plot(xx, yy, 'k-')
+    plt.plot(xx, yy_down, 'k--')
+    plt.plot(xx, yy_up, 'k--')
+
+    plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=500,
+                facecolors='none', zorder=10, edgecolors='k')
+    plt.scatter(X[:, 0], X[:, 1], c=Y, zorder=10, cmap=plt.cm.Paired,s=500,
+                edgecolors='k')
+
+    plt.axis('tight')
+    x_min = -4.8
+    x_max = 4.2
+    y_min = -6
+    y_max = 6
+
+    XX, YY = np.mgrid[x_min:x_max:200j, y_min:y_max:200j]
+    Z = clf.predict(np.c_[XX.ravel(), YY.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(XX.shape)
+    plt.figure(fignum, figsize=(4, 3))
+    plt.pcolormesh(XX, YY, Z, cmap=plt.cm.Paired)
+
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+
+    plt.xticks(())
+    plt.yticks(())
+    fignum = fignum + 1
+
+    plt.show()
