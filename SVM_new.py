ea0db41ec2de38122c1725713ea91f83230e282c
Meera Murali <mmurali@pdx.edu>
2018-06-06 17:17:48 -0700

    Final files for SVM (2000, 200, and 100 features) (includes accuracy, confusion matrix, decision region plots, and precision-recall curve)

---
+++ SVM_new.py
@@ -0,0 +1,80 @@
+from sklearn import svm,metrics
+import numpy as np
+import matplotlib as plt
+from sklearn.decomposition import PCA
+import datetime
+
+from mlxtend.plotting import plot_decision_regions
+
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import average_precision_score
+
+
+
+if __name__ == "__main__":
+    startTime = datetime.datetime.now()
+
+    # Load training data
+    x = np.load('data/train_encoded_array_new.npy')
+    y = np.load('data/train_target_array_new.npy')
+    y = y.astype('int')
+    y = y.flatten()
+
+    # Load test data
+    z = np.load('data/test_encoded_array_new.npy')
+    t = np.load('data/test_target_array_new.npy')
+    t = t.astype('int')
+    t = t.flatten()
+
+
+    # Predict using SVM
+    clf = svm.SVC()
+    clf.fit(x, y)
+    p = clf.predict(z)
+
+    # Compute training time
+    endTime = datetime.datetime.now() - startTime
+    print("Total time taken to train: ", endTime)
+    print("\n")
+
+
+    print("SVM with 2000 features")
+
+    # Compute accuracy
+    accuracy = metrics.accuracy_score(t, p, normalize=False)
+    print("Accuracy: ", (accuracy/len(t)) * 100)
+
+    # Confusion matrix
+    confusion_matrix = metrics.confusion_matrix(t, p)
+    print("Confusion Matrix:\n", confusion_matrix)
+
+    # Plotting decision regions
+    plot_decision_regions(z, p, clf=clf, legend=2)
+    plt.xlabel('X0')
+    plt.ylabel('X1')
+    plt.title('SVM with 2000 features')
+    plt.show()
+    plt.savefig('data/svm2000d_decisionRegions')
+
+    # Replace 4s with 1s
+    t[np.where(t==4)] = 1
+    p[np.where(p==4)] = 1
+
+    # Plot the Precision-Recall curve
+    precision, recall, _ = precision_recall_curve(t, p)
+    plt.step(recall, precision, color='b', alpha=0.2, where='post')
+    plt.fill_between(recall, precision, step='post', alpha=0.2, color='b')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    average_precision = average_precision_score(t, p)
+    plt.title('SVM (2000d) Precision-Recall curve: AP={0:0.2f}'.format(average_precision))
+    plt.show()
+    plt.savefig('data/svm2000d_precisionRecall')
+
+
+
+
+
+
